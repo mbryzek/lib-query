c4dff3009c405399d8d2776dfd5262a70614f765
Michael Bryzek <mbryzek@alum.mit.edu>
2024-10-29 11:27:59 -0400

    wip

--- src/test/scala/helpers/BaseSpec.scala
+++ src/test/scala/helpers/BaseSpec.scala
@@ -3,4 +3,4 @@ package helpers
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-abstract class OrderBySpec extends AnyWordSpec with Matchers with TestHelpers
+abstract class BaseSpec extends AnyWordSpec with Matchers with TestHelpers
