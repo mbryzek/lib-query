8e68ed7b89041933b38c92f55e6e4d18f23a084c
Michael Bryzek <mbryzek@alum.mit.edu>
2026-05-03 11:44:34 -0400

    Add LocalTime support to Parameter (#4)

--- src/test/scala/com/bryzek/util/ParameterSpec.scala
+++ src/test/scala/com/bryzek/util/ParameterSpec.scala
@@ -1,6 +1,7 @@
 package com.bryzek.util
 
 import helpers.BaseSpec
+import org.joda.time.LocalTime
 import Parameter.*
 
 class ParameterSpec extends BaseSpec {
@@ -27,6 +28,20 @@ class ParameterSpec extends BaseSpec {
       param mustBe a[TypeBytes]
       param.value mustBe "\\x0102"
     }
+
+    "handle LocalTime" in {
+      val param = Parameter.from(new LocalTime(11, 0))
+      param mustBe a[TypeLocalTime]
+      param.value mustBe "11:00:00.000"
+      param.interpolationValue mustBe "'11:00:00.000'"
+      param.cast mustBe Some("time")
+    }
+
+    "handle Option[LocalTime]" in {
+      val param = Parameter.from(Some(new LocalTime(13, 30, 45)))
+      param mustBe a[TypeLocalTime]
+      param.value mustBe "13:30:45.000"
+    }
   }
 
   "bind with bytes" in {
@@ -34,5 +49,12 @@ class ParameterSpec extends BaseSpec {
     val q = Query("insert into files (data) values ({data}::bytea)").bind("data", bytes)
     q.sql() mustBe "insert into files (data) values ({data}::bytea)"
     q.interpolate() mustBe "insert into files (data) values ('\\x4865')"
+  }
+
+  "bind with LocalTime" in {
+    val q = Query("insert into reservations (start_time) values ({start_time}::time)")
+      .bind("start_time", new LocalTime(11, 0))
+    q.sql() mustBe "insert into reservations (start_time) values ({start_time}::time)"
+    q.interpolate() mustBe "insert into reservations (start_time) values ('11:00:00.000')"
   }
 }
